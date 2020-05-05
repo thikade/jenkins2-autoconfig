54ebbcf099bb5213cb2efab61db4f3aeb764c24b
thikade <thomas.hikade@gmail.com>
2020-05-05 17:38:26 +0200

    update

--- pipelines/ansible.groovy
+++ pipelines/ansible.groovy
@@ -4,8 +4,7 @@ import groovy.json.JsonOutput
 String PREFIX = "stiu"
 
 // main data structure -  will be read from Ansible 
-// List STAGES = [ "uat", "prod"]
-List STAGES = [ "uat" ]
+List STAGES = [ "uat", "prod"]
 
 Map ansibleVariables = [
     'repo': [
