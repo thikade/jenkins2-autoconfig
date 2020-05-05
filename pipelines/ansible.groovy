64d5f3c78cf906e172892e4e57e19101bc4a5e3f
thikade <thomas.hikade@gmail.com>
2020-05-06 00:24:34 +0200

    update

--- pipelines/ansible.groovy
+++ pipelines/ansible.groovy
@@ -87,7 +87,7 @@ Map ansibleVariables = [
 
 // Ansible Extra Variables: added via -e KEY=VALUE -e KEY2=VALUE2
 def ansibleExtraVars = [
-    "WORKSPACE_DIR" : WORKSPACE
+    "WORKSPACE_DIR" : "${WORKSPACE}"
 ]
 
 // Ansible additional commandline args
